f75b2ce61b6ed329739e14293298773b1f9ee210
aivaras <aivaras@lupasearch.com>
2023-11-17 15:36:52 +0200

    Switch up how base plugin is installed for bettter support

--- src/runtime/lupaSearch.ts
+++ src/runtime/lupaSearch.ts
@@ -2,5 +2,5 @@ import { LupaSearch } from "@getlupa/vue";
 import { defineNuxtPlugin } from "nuxt/app";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(LupaSearch);
+  LupaSearch.install(nuxtApp.vueApp)
 });
